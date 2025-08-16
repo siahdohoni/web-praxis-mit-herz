59cc5aee4fbe265d7b42db31ae0f4bcf3e534ff1
Darjush Siahdohoni <darjush@siahdohoni.com>
2025-08-17 00:06:55 +0200

    Add tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()]
+  integrations: [mdx()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
