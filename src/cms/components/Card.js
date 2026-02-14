bd27f504a2bcdd099c37084d28909842ebf76471
Darjush Siahdohoni <darjush@siahdohoni.com>
2026-02-14 20:43:04 +0100

    Update Card and ImageBlock components

--- src/cms/components/Card.js
+++ src/cms/components/Card.js
@@ -4,13 +4,12 @@ export const Card = {
   id: 'card',
   label: 'Card',
   fields: [
-    {label: 'Title', name: 'title', widget: 'string', required: false},
     {label: 'Image', name: 'image', widget: 'image'},
     {label: 'Alt Text', name: 'alt', widget: 'string', required: false},
     {label: 'Link (URL)', name: 'href', widget: 'string', required: false},
     {label: 'Body', name: 'body', widget: 'markdown'},
   ],
-  // Match <Card title="..." image="..." body="..." href="..." alt="...">content</Card>
+  // Match <Card image="..." href="..." alt="...">content</Card>
   pattern: /^\s*<Card\s+([^>]*?)>([\s\S]*?)\s*<\/Card>/m,
   fromBlock: function (match) {
     return {
@@ -19,8 +18,8 @@ export const Card = {
     };
   },
   toBlock: function (obj) {
-    const { body, ...rest } = obj;
-    return `<Card ${toAttrString(rest, ['title', 'image', 'href', 'alt'])}>
+    const {body, ...rest} = obj;
+    return `<Card ${toAttrString(rest, ['image', 'href', 'alt'])}>
 ${body.trim() || ''}
 </Card>`;
   },
@@ -28,8 +27,8 @@ ${body.trim() || ''}
     return `
 <div class="ce_text grid1 block">
   <figure class="image_container float_above">
-    <a href="${obj.href || ''}" title="${obj.title || ''}">
-      <img src="${obj.image}" alt="${obj.alt || ''}" style="width: 300px;" />
+    <a href="${obj.href || ''}" title="${obj.alt || ''}">
+      <img src="${obj.image}" alt="${obj.alt || ''}" title="${obj.alt || ''}" style="width: 300px;" />
     </a>
   </figure>
     ${obj.body || ''}
--- src/cms/components/ImageBlock.js
+++ src/cms/components/ImageBlock.js
@@ -1,31 +1,35 @@
-import { parseAttrs, toAttrString } from '../utils.js';
+import {parseAttrs, toAttrString} from '../utils.js';
 
 export const ImageBlock = {
-    id: 'imageblock',
-    label: 'ImageBlock',
-    fields: [
-        {label: 'Title', name: 'title', widget: 'string', required: false},
-        {label: 'Image', name: 'image', widget: 'image'},
-        {label: 'Alt Text', name: 'alt', widget: 'string', required: false},
-        {label: 'Body', name: 'body', widget: 'markdown'},
-    ],
-    // Match <ImageBlock title="..." image="..." body="..." alt="..." />
-    pattern: /^<ImageBlock\s+([\s\S]*?)\s*\/>/m,
-    fromBlock: function (match) {
-        return parseAttrs(match[1]);
-    },
-    toBlock: function (obj) {
-        return `<ImageBlock ${toAttrString(obj)} />`;
-    },
-    toPreview: function (obj) {
-        return `
+  id: 'imageblock',
+  label: 'ImageBlock',
+  fields: [
+    {label: 'Image', name: 'image', widget: 'image'},
+    {label: 'Alt Text', name: 'alt', widget: 'string', required: false},
+    {label: 'Body', name: 'body', widget: 'markdown'},
+  ],
+  // Match <ImageBlock image="..." alt="...">content</ImageBlock>
+  pattern: /^\s*<ImageBlock\s+([^>]*?)>([\s\S]*?)\s*<\/ImageBlock>/m,
+  fromBlock: function (match) {
+    return {
+      ...parseAttrs(match[1]),
+      body: match[2]
+    };
+  },
+  toBlock: function (obj) {
+    const {body, ...rest} = obj;
+    return `<ImageBlock ${toAttrString(rest, ['image', 'href', 'alt'])}>
+${body.trim() || ''}
+</ImageBlock>`;
+  },
+  toPreview: function (obj) {
+    return `
             <div class="ce_text block">
               <figure class="image_container float_right">
                 <img src="${obj.image}" alt="${obj.alt || ''}" style="width: 300px;" />
               </figure>
-              ${obj.title ? `<h1>${obj.title}</h1>` : ''}
               <div>${obj.body || ''}</div>
             </div>
         `;
-    }
+  }
 };
