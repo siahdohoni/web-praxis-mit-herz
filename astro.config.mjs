5c3dcde7c386a14e5d7b0dbabbb050760496a62d
Darjush Siahdohoni <darjush@siahdohoni.com>
2026-02-18 19:22:52 +0100

    uninstall @tailwindcss/vite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
 });
