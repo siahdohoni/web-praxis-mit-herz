3fd85dff53b0077af74169cd919316aabcb5de0f
Darjush Siahdohoni <darjush@siahdohoni.com>
2026-02-24 00:13:43 +0100

    Use .net for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.praxismitherz.de",
+  site: "https://www.praxismitherz.net",
   integrations: [mdx()],
 });
